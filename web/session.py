07f291e2ef7a73f11a2c295ebcbbceb5a1d45f81
zhouyu <ballackscholl@gmail.com>
2015-02-15 23:40:12 +0800

    add no web no session

--- web/session.py
+++ web/session.py
@@ -51,6 +51,8 @@ class Session(object):
         "store", "_initializer", "_last_cleanup_time", "_config", "_data", 
         "__getitem__", "__setitem__", "__delitem__"
     ]
+
+    __CLIENT_AGENT_LIST = ['MSIE', 'Firefox', 'Chrome', 'Safari', 'Opera', 'UCWEB']
     
 
     def __init__(self, app, store, initializer=None):
@@ -66,6 +68,12 @@ class Session(object):
 
         if app:
             app.add_processor(self._processor)
+
+    def __isBrowser(self):
+        for client in Session.__CLIENT_AGENT_LIST:
+            if web.ctx.env['HTTP_USER_AGENT'].find(client) != -1:
+                return True
+        return False
 
     def __contains__(self, name):
         return name in self._data
@@ -94,6 +102,16 @@ class Session(object):
 
     def _load(self):
         """Load the session from the store, by the id from cookie"""
+
+        if not 'HTTP_USER_AGENT' in web.ctx.env:
+            self.isBrowser = False
+            return
+
+        if not self.__isBrowser():
+            self.isBrowser = False
+            return
+
+        self.isBrowser = True
 
         cookie_name = self._config.cookie_name
         cookie_domain = self._config.cookie_domain
@@ -137,6 +155,8 @@ class Session(object):
                return self.expired() 
     
     def _save(self):
+        if not self.isBrowser:
+            return
 
         if not self.get('_killed'):
             if self._config.keep_cookie:
